eb44dfac48f808c0e652868f3d10c03b130ee1fa
Octavio Cornejo <ocornejo@arkusnexus.com>
2021-04-26 15:03:26 -0500

    Testing Elasticsearch functionality.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,20 @@ class HomeController < ApplicationController
 
   def index
   end
+
+  def search
+    result = Location.search(search_params[:q], search_params)
+
+    locations = result.map do |r|
+      r.merge(r.delete('_source')).merge('id': r.delete('_id'))
+    end
+
+    render json: { locations: locations }, status: :ok
+  end
+
+  private
+
+  def search_params
+    params.permit(:q, :level)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '*path', to: 'home#index'
-
+  
   get 'home/index'
+  get 'home/search'
   root 'home#index'
-
+  
+  get '*path', to: 'home#index'
 end
